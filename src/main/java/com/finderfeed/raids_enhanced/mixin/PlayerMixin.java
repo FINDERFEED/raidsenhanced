591d068fd0ee9460739524da0ce6dff33fb66c50
FINDERFEED <windingaster335@gmail.com>
2026-02-11 06:59:13 +0300

    player blimp controls

--- src/main/java/com/finderfeed/raids_enhanced/mixin/PlayerMixin.java
+++ src/main/java/com/finderfeed/raids_enhanced/mixin/PlayerMixin.java
@@ -3,6 +3,7 @@ package com.finderfeed.raids_enhanced.mixin;
 import com.finderfeed.raids_enhanced.content.entities.player_blimp.PlayerBlimpEntity;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,6 +13,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public class PlayerMixin {
+
+    @Inject(method = "updatePlayerPose", at = @At("HEAD"), cancellable = true)
+    public void updatePlayerPose(CallbackInfo ci){
+        Player player = (Player) (Object) this;
+        if (player.getVehicle() instanceof PlayerBlimpEntity playerBlimp){
+            player.setPose(Pose.STANDING);
+            ci.cancel();
+        }
+    }
 
     @Inject(method = "wantsToStopRiding", at = @At("HEAD"), cancellable = true)
     public void wantsToStopRiding(CallbackInfoReturnable<Boolean> cir){
