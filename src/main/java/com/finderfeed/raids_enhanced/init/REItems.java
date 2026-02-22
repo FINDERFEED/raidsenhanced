4ec1fc672baacd67baeacc646ca04d9bbd1a4661
FINDERFEED <windingaster335@gmail.com>
2026-02-22 11:21:24 +0300

    spawn eggs and fixes
    (cherry picked from commit a478697dd65710fad5f1333bb4322d86e4685043)

--- src/main/java/com/finderfeed/raids_enhanced/init/REItems.java
+++ src/main/java/com/finderfeed/raids_enhanced/init/REItems.java
@@ -11,17 +11,25 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraft.world.item.SpawnEggItem;
+import net.neoforged.neoforge.common.DeferredSpawnEggItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
 
 public class REItems {
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, RaidsEnhanced.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.createItems(RaidsEnhanced.MOD_ID);
 
     public static final Supplier<Item> BLIMP_PARTS = ITEMS.register("blimp_parts", ()->new ItemWithDescription(new Item.Properties().stacksTo(1), Component.translatable("raidsenhanced.item_description.blimp_parts").withStyle(ChatFormatting.GOLD)));
     public static final Supplier<Item> PLAYER_BLIMP = ITEMS.register("player_blimp", ()->new PlayerBlimpItem(new Item.Properties().stacksTo(1)));
     public static final Supplier<Item> DEBUG_STICK = ITEMS.register("debug_stick", ()->new REDebugStick(new Item.Properties().stacksTo(1)));
     public static final Supplier<ZapperStaff> ZAPPER_STAFF = ITEMS.register("zapper_staff", ()->new ZapperStaff(new Item.Properties().stacksTo(1)));
     public static final Supplier<HandCannon> HANDCANNON = ITEMS.register("handcannon", ()->new HandCannon(new Item.Properties().stacksTo(1)));
+
+    public static final Supplier<SpawnEggItem> ZAPPER_SPAWN_EGG = ITEMS.register("zapper_spawn_egg", ()->new DeferredSpawnEggItem(REEntities.ZAPPER_ILLAGER,0xffffff,0xffffff,new Item.Properties().stacksTo(1)));
+    public static final Supplier<SpawnEggItem> RAID_BLIMP_SPAWN_EGG = ITEMS.register("raid_blimp_spawn_egg", ()->new DeferredSpawnEggItem(REEntities.RAID_BLIMP,0xffffff,0xffffff,new Item.Properties().stacksTo(1)));
+    public static final Supplier<SpawnEggItem> RAID_DRILL_SPAWN_EGG = ITEMS.register("raid_drill_spawn_egg", ()->new DeferredSpawnEggItem(REEntities.RAID_DRILL,0xffffff,0xffffff,new Item.Properties().stacksTo(1)));
+    public static final Supplier<SpawnEggItem> GOLEM_OF_LAST_RESORT_SPAWN_EGG = ITEMS.register("golem_of_last_resort_spawn_egg", ()->new DeferredSpawnEggItem(REEntities.GOLEM_OF_LAST_RESORT,0xffffff,0xffffff,new Item.Properties().stacksTo(1)));
 
 }
